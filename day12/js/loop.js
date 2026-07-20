ebff97c82f0297e99ce53a3fd3f1ebe5a808394b
Omar Eldawoudy <eldawoudyomar55@gmail.com>
2026-07-21 02:15:17 +0300

    Task - JS Basics Part 1

---
+++ day12/js/loop.js
@@ -0,0 +1,100 @@
+// //!------ if condition-------
+// var age = 16;
+// var country = "USA"
+// if (country == "Egypt") {
+//     if (age >= 18) {
+
+//         console.log("you can drive");
+//     }
+//     else {
+
+//         console.log("you can't drive");
+
+//     }
+// }
+// else if (country == "USA") {
+//     if (age >= 16) {
+//         console.log("you can drive");
+//     }
+//     else {
+//         console.log("you can't drive");
+
+//     }
+// }
+// else {
+
+//     console.log("Country not supported");
+
+// }
+
+
+// //*---------------Switch-----------------------------
+
+// var lang="Css";
+// switch (lang )
+// {
+//     case "Html":
+//         console.log("2 Day");
+//         break;
+    
+//     case "Css":
+//         console.log("5 Day");
+//         break;
+    
+//     case "js":
+//         console.log("20 Day");
+//         break;
+    
+//     case "react":
+//         console.log("15 day");
+//         break;
+// }
+
+// !-----loop-----------
+
+for (var i = 1; i<=12; i++)
+{
+    console.log(`7 * ${i} =${7 * i}`);
+}
+
+var i = 0;
+do {
+    console.log(`the counter is ${i}`);
+} while (i<10);
+
+//----------------------
+(function call() {
+    console.log = (`hello omar`);    
+
+})()
+
+var user = {
+    full_name: "omar ismail",
+    age: 21,
+    job: "developer",
+    salary: 100000,
+    married: false,
+    eat: function (meal)
+    {
+        console.log(`iam eating ${meal}`);
+    },
+
+    bro: {
+        full_name: "Mo7med ismail",
+        age: 30,
+        job: "Eng",
+        salary: 1000000000,
+        married: true
+    },
+
+};
+console.log(user.full_name);
+console.log(user.job);
+
+console.log(user.bro.full_name);
+console.log(user.bro.job);
+console.log(user.eat("salad"));
+
+
+
+
